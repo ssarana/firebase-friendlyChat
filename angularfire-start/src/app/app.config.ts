780c43f4b8e05452be99e62a474bb2af6557821b
Sandip-CR7 <ssarana@student.tgm.ac.at>
2025-12-12 12:55:18 +0100

    config nochmal geändert

--- angularfire-start/src/app/app.config.ts
+++ angularfire-start/src/app/app.config.ts
@@ -13,11 +13,11 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideFirebaseApp(() => initializeApp({
     apiKey: "AIzaSyBi2cSLs2F1YNdK-gbbTSkOw3WpqXLtXew",
-  authDomain: "friendlychat-57563.firebaseapp.com",
-  projectId: "friendlychat-57563",
-  storageBucket: "friendlychat-57563.firebasestorage.app",
-  messagingSenderId: "763190858328",
-  appId: "1:763190858328:web:8a061b3ec7f852daeb702d" 
+    authDomain: "friendlychat-57563.firebaseapp.com",
+    projectId: "friendlychat-57563",
+    storageBucket: "friendlychat-57563.firebasestorage.app",
+    messagingSenderId: "763190858328",
+    appId: "1:763190858328:web:8a061b3ec7f852daeb702d" 
     })),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
