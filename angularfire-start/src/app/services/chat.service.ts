1d2677ffe830aefe5c1445e3a7ff951ad18a21f3
Sandip-CR7 <ssarana@student.tgm.ac.at>
2025-12-12 13:08:05 +0100

    Adding Google Authentication

--- angularfire-start/src/app/services/chat.service.ts
+++ angularfire-start/src/app/services/chat.service.ts
@@ -66,7 +66,7 @@ export class ChatService {
   user$ = user(this.auth);
   currentUser: User | null = this.auth.currentUser;
   userSubscription: Subscription;
-  
+
   constructor() {
     this.userSubscription = this.user$.subscribe((aUser: User | null) => {
         this.currentUser = aUser;
@@ -74,10 +74,25 @@ export class ChatService {
   }
 
   // Login Friendly Chat.
-  login() {}
+  // Signs-in Friendly Chat.
+  login() {
+    signInWithPopup(this.auth, this.provider).then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      this.router.navigate(['/', 'chat']);
+      return credential;
+    })
+  }
 
   // Logout of Friendly Chat.
-  logout() {}
+  // Logout of Friendly Chat.
+  logout() {
+    signOut(this.auth).then(() => {
+      this.router.navigate(['/', 'login'])
+      console.log('signed out');
+    }).catch((error) => {
+      console.log('sign out error: ' + error);
+    })
+  }
 
   // Adds a text or image message to Cloud Firestore.
   addMessage = async (
