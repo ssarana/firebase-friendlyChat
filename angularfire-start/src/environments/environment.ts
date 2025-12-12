20458684622a982cae5b16e74e0e03ca09a74a28
Sandip-CR7 <ssarana@student.tgm.ac.at>
2025-12-12 12:46:43 +0100

    enviroment.ts geändert

--- angularfire-start/src/environments/environment.ts
+++ angularfire-start/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   projectId: "friendlychat-57563",
   storageBucket: "friendlychat-57563.firebasestorage.app",
   messagingSenderId: "763190858328",
-  appId: "1:763190858328:web:8a061b3ec7f852daeb702d"
+  appId: "1:763190858328:web:8a061b3ec7f852daeb702d",
     measurementId: 'G-MEASUREMENT_ID',
   },
 };
